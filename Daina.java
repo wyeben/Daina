9af570d33c6a10ff9cda538d053d4f1b7cec5c73
Yila Benson <134516892+wyeben@users.noreply.github.com>
2023-06-04 09:51:32 +0100

    Add files via upload

---
+++ Daina.java
@@ -0,0 +1,16 @@
+class Daina {
+
+   public static void main(String[] args){
+   
+ System.out.print("Hello Daina baby\n");
+ 
+ System.out.print("oh how much i love\n");
+ 
+ System.out.print("to see your beautiful face.\n");
+ 
+ System.out.print("You are the woman of my dreams\n");
+ 
+ System.out.print("God bless the day i met you.\n");
+ 
+ }
+}
